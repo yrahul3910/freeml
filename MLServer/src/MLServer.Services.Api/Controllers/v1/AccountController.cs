f42f5bec8bb4144d10e9398bd0911e4d7a4bbaa9
Rahul Yedida <rahul.yedida@pm.me>
2023-01-30 00:14:33 -0500

    Implemented auth, updated client

---
+++ MLServer/src/MLServer.Services.Api/Controllers/v1/AccountController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using MLServer.Domain.Core.Bus;
+using MLServer.Domain.Core.Notifications;
+using MLServer.Domain.Interfaces;
+using MLServer.Infra.CrossCutting.Identity.Models;
+using MLServer.Services.Api.Configurations;
+
+namespace MLServer.Services.Api.Controllers.v1
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/v1/account")]
+    [Produces("application/json")]
+    public class AccountController : ApiController
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly AppSettings _appSettings;
+        private readonly IUser _user;
+
+        public AccountController(
+            SignInManager<IdentityUser> signInManager,
+            UserManager<IdentityUser> userManager,
+            IOptions<AppSettings> appSettings,
+            IUser user,
+            INotificationHandler<DomainNotification> notifications,
+            IMediatorHandler mediator)
+            : base(notifications, mediator)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _appSettings = appSettings.Value;
+            _user = user;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("login")]
+        public async Task<IActionResult> Login(UserLogin userLogin)
+        {
+            if (!ModelState.IsValid)
+            {
+                NotifyModelStateErrors();
+                return Response(userLogin);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(userLogin.Email, userLogin.Password, false, true);
+
+            if (result.Succeeded)
+            {
+                var token = await GenerateJwt(userLogin.Email);
+                return Ok(new
+                {
+                    access_token = token,
+                    token_type = "bearer",
+                    expires_in = DateTime.UtcNow.AddHours(_appSettings.Expiration),
+                    userName = userLogin.Email
+                });
+            }
+
+            NotifyError("Login", result.ToString());
+            return Response(userLogin);
+        }
+
+        [HttpGet]
+        [Route("UserInfo")]
+        public IActionResult GetUserInfo()
+        {
+            if (_user.IsAuthenticated())
+                return Ok(new
+                {
+                    Nome = _user.Name,
+                    Sobrenome = "",
+                    Email = _user.Name
+                });
+
+            return BadRequest();
+        }
+
+        private async Task<string> GenerateJwt(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            var claims = await _userManager.GetClaimsAsync(user);
+
+            claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.Id));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
+
+            var identityClaims = new ClaimsIdentity();
+            identityClaims.AddClaims(claims);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = identityClaims,
+                Issuer = _appSettings.Issuer,
+                Audience = _appSettings.ValidAt,
+                Expires = DateTime.UtcNow.AddHours(_appSettings.Expiration),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            return tokenHandler.WriteToken(tokenHandler.CreateToken(tokenDescriptor));
+        }
+    }
+}
--- client/src/main.py
+++ client/src/main.py
@@ -1,31 +1,66 @@
 import argparse
 import os
 
-import cutie
 import requests
 from tabulate import tabulate
 
-from utils import fatal_error
+from utils import fatal_error, info
 
 
 if 'FREEML_MODE' not in os.environ or os.environ['FREEML_MODE'] == 'dev':
-    BASE_URL = 'http://localhost:5000'
+    BASE_URL = 'https://localhost:5001'
 else:
     pass
     # TODO: Set the prod URL
 
 
-def list():
+if 'localhost' in BASE_URL:
+    verify_cert = False
+else:
+    verify_cert = True
+
+
+def login(email: str, password: str):
+    """
+    Authenticates the user.
+    """
+
+    response = requests.post(f'{BASE_URL}/api/v1/account/login', json={
+        'email': email,
+        'password': password
+    }, verify=verify_cert)
+
+    if response.status_code != 200:
+        fatal_error('Invalid credentials')
+    
+    info('Successfully logged in.')
+    return response.json()['access_token']
+
+
+def status_to_text(status: int):
+    """
+    Converts a status code to a human-readable string.
+    """
+    match status:
+        case 0: return 'Pending'
+        case 1: return 'Running'
+        case 2: return 'Completed'
+
+
+def list(token: str):
     """
     Lists available jobs, along with their descriptions.
     """
-    response = requests.get(f'{BASE_URL}/jobs/list')
+    response = requests.get(f'{BASE_URL}/api/v1/job', 
+        headers={'Authorization': f'Bearer {token}'},
+        verify=verify_cert)
 
     if response.status_code != 200:
         fatal_error('Failed to list jobs')
 
-    response = response.json()['jobs']
-    print(tabulate(response, headers=['Name', 'Description']))
+    response = response.json()
+    response = [(x['id'], x['name'], x['description'], status_to_text(x['status'])) for x in response]
+    print(tabulate(response, headers=['ID', 'Name', 'Description', 'Status']))
 
 
 def _main():
@@ -33,14 +68,25 @@ def _main():
         prog='FreeML Client',
         description='A command line client for OpenML'
     )
+    parser.add_argument('--email', help='Email address to use', required=True)
+    parser.add_argument('--password', help='Password to use', required=True)
     parser.add_argument('command', help='Subcommand to run')
     args = parser.parse_args()
     
     if args.command not in globals():
         fatal_error('Invalid command')
+    
+    email = args.email
+    password = args.password
+
+    # Authenticate
+    if email is None or password is None:
+        fatal_error("Invalid credentials.")
+    
+    token = login(email, password)
 
     # Run the command
-    globals()[args.command]()
+    globals()[args.command](token)
 
 
 if __name__ == '__main__':
